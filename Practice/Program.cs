2c6c03f97a9784a61dd1a8861d0ab6924e174843
Chandragupt1 <chandra.gupta2506@gmail.com>
2024-07-26 11:21:49 +0530

    Add files via upload

---
+++ Practice/Program.cs
@@ -0,0 +1,17 @@
+﻿//loop:
+using System;
+namespace Practice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+       
+            {
+            int i;
+                for (i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+        }
+    }
+}
